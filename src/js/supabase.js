71a0f19f4e3763c19ec6aa3c5fa1d46520b657cc
Hina-Fayyaz <hinafayyazofficial04@gmail.com>
2026-07-14 07:09:36 +0500

    Delete src/js/supabase.js

--- src/js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://opwbpmqpgudwzssvkotk.supabase.co';
-const SUPABASE_ANON_KEY = 'opwbpmqpgudwzssvkotk';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
